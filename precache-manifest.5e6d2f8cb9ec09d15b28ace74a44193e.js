19e8d47d94dc5b981ebb5e272dc1e94c1afd4a13
MHMighani <mh.mgh2012@gmail.com>
2020-02-20 10:14:40 +0330

    Updates

---
+++ precache-manifest.5e6d2f8cb9ec09d15b28ace74a44193e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f1ed3612118ae23caa7261a2542892",
+    "url": "/tic-toc-toe-react/index.html"
+  },
+  {
+    "revision": "392e49e9707b64e7f902",
+    "url": "/tic-toc-toe-react/static/css/main.6d68d006.chunk.css"
+  },
+  {
+    "revision": "6aa2616601cc465a4c74",
+    "url": "/tic-toc-toe-react/static/js/2.f85936f0.chunk.js"
+  },
+  {
+    "revision": "392e49e9707b64e7f902",
+    "url": "/tic-toc-toe-react/static/js/main.951bb475.chunk.js"
+  },
+  {
+    "revision": "d26557d85637ec6ad0bd",
+    "url": "/tic-toc-toe-react/static/js/runtime~main.569868b5.js"
+  }
+]);
